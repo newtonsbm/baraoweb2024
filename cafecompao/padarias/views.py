fa91874a5e2144379c608005b391007061f1628c
Newton Miyoshi <newton.miyoshi@medway.com.br>
2024-03-29 00:33:46 -0300

    feat: djagno project initial

---
+++ cafecompao/padarias/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse('Bem vindo ao Cafe com Pão')
+
+def about(request):
+    return HttpResponse('Sobre o Café com Pão')
+
+def contact(request):
+    return HttpResponse('Entre em contato')
