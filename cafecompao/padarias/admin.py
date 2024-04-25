b8eaa442849f05c342c96010cb33d103c99f8d2c
Newton Miyoshi <newton.miyoshi@medway.com.br>
2024-04-25 20:05:38 -0300

    feat: atividade 5

--- cafecompao/padarias/admin.py
+++ cafecompao/padarias/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Categoria, Produto, Cesta, Padaria
 
 # Register your models here.
+admin.site.register(Categoria)
+admin.site.register(Produto)
+admin.site.register(Cesta)
+admin.site.register(Padaria)
+
