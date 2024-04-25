1cd8548423c639e93e39d1d18cc077ce3f67389d
Newton Miyoshi <newton.miyoshi@medway.com.br>
2024-04-25 19:21:19 -0300

    feat: atividade 4

--- cafecompao/padarias/models.py
+++ cafecompao/padarias/models.py
@@ -1,8 +1,46 @@
+import uuid
 from django.db import models
 
 class Categoria(models.Model):
     nome = models.CharField(
         verbose_name="Nome", max_length=100, unique=True, null=False, blank=False, help_text="Nome da categoria")
+
+    def __str__(self):
+        return self.nome
+
+class Produto(models.Model):
+    nome = models.CharField(
+        verbose_name="Nome", max_length=100, unique=True, null=False, blank=False, help_text="Nome do produto")
+    descricao = models.TextField(
+        verbose_name="Descrição", null=True, blank=True, help_text="Descrição do produto")
+    preco = models.DecimalField(
+        verbose_name="Preço", max_digits=10, decimal_places=2, null=False, blank=False, help_text="Preço do produto")
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.SET_NULL, verbose_name="Categoria", null=True, help_text="Categoria do produto")
+
+    def __str__(self):
+        return self.nome
+
+class Cesta(models.Model):
+
+    class Niveis(models.TextChoices):
+        BASICO = 'B', 'Básico'
+        MEDIO = 'M', 'Médio'
+        PREMIUM = 'P', 'Premium'
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    nome = models.CharField(
+        verbose_name="Nome", max_length=100, unique=True, null=False, blank=False, help_text="Nome da cesta")
+    descricao = models.TextField(
+        verbose_name="Descrição", null=True, blank=True, help_text="Descrição da cesta")
+    preco = models.DecimalField(
+        verbose_name="Preço", max_digits=10, decimal_places=2, null=False, blank=False, help_text="Preço da cesta")
+    produtos = models.ManyToManyField(
+        Produto, verbose_name="Produtos", help_text="Produtos da cesta", related_name="cestas")
+    nivel = models.CharField(
+        verbose_name="Nível", max_length=1, choices=Niveis.choices, default=Niveis.BASICO, help_text="Nível da cesta")
+    imagem = models.ImageField(
+        verbose_name="Imagem", upload_to="cestas", null=True, blank=True, help_text="Imagem da cesta")
 
     def __str__(self):
         return self.nome
