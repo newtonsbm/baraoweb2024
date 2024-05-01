61f08f4d343cc170f9c983f64d05def525f6c14a
Newton Miyoshi <newton.miyoshi@medway.com.br>
2024-05-01 18:44:26 -0300

    feat: atividade 6 context

--- cafecompao/padarias/views.py
+++ cafecompao/padarias/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-# Create your views here.
+# importar os models
+from .models import Padaria, Produto, Cesta
 
 def home(request): 
-    return render(request, 'home.html')
+    qtd_padarias = Padaria.objects.count()
+    qtd_produtos = Produto.objects.count()
+    qtd_cestas = Cesta.objects.count()
+    context = {
+        'qtd_padarias': qtd_padarias,
+        'qtd_produtos': qtd_produtos,
+        'qtd_cestas': qtd_cestas,
+    }
+    return render(request, 'home.html', context)
 
 def about(request):
     return HttpResponse('Sobre o Café com Pão')
