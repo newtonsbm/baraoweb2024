b135d89d35abab5abb7f6caa97748ce8ab091ccb
Newton Miyoshi <newton.miyoshi@medway.com.br>
2024-05-23 21:17:57 -0300

    Update settings.py with dynamic allowed hosts
    Add the environment variable `FLY_APP_NAME` to the `settings.py` file and use it to dynamically set the `ALLOWED_HOSTS` list. This ensures that the application only accepts requests from the specified host.
    
    Add `FLY_APP_NAME` to the environment variables and update the `ALLOWED_HOSTS` list in `settings.py` to include the value of `FLY_APP_NAME`.
    
    Add `FLY_APP_NAME` to the environment variables and update the `ALLOWED_HOSTS` list in `settings.py` to include the value of `FLY_APP_NAME`.

--- cafecompao/cafecompao/settings.py
+++ cafecompao/cafecompao/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import os
 
 from pathlib import Path
+
+
+APP_NAME = os.environ.get("FLY_APP_NAME")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +29,7 @@ SECRET_KEY = 'django-insecure-2r)m$2-ejc%ooeyq4@#3aewp6ym8*jzvxno%+spk0aug5e_r2x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [f"{APP_NAME}.fly.dev"]
 
 
 # Application definition
